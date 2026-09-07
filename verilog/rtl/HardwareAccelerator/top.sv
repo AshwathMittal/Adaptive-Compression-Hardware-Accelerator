310f78afc194c063a7a1985e41cda174cebf4a9d
Ashwath Mittal <ashwathmittal@gmail.com>
2026-09-07 21:28:33 +0000

    Created Design Template

---
+++ verilog/rtl/HardwareAccelerator/top.sv
@@ -0,0 +1,14 @@
+module top #(
+    //parameters here
+)(
+    input logic clk, nrst, //clock and negative-edge reset
+    //other signals here
+    output logic out
+);
+    //internal signals
+    logic a;
+    assign out = a;
+
+    //all submodule instantiations here
+    
+endmodule
