99b7ac19d2be9136f06d8efc2c9d10154511578a
Ashwath Mittal <68093378+AshwathMittal@users.noreply.github.com>
2026-09-07 17:10:35 -0400

    Initial commit

---
+++ verilog/rtl/reset.sv
@@ -0,0 +1,19 @@
+//TODO: PLEASE REMOVE FROM TEMPLATE REPO
+`default_nettype none
+logic clk;
+logic syncrst;
+logic D, Q;
+ 
+module reset (
+  input logic clk,
+  input logic rst_button,
+  output rst
+);
+ 
+always_ff @(posedge clk) begin
+  if (rst) begin
+    Q <= 0;
+  end else begin
+    Q <= D;
+  end
+end
